18525dd7ae2f69881cb6df5b2f05b327807ef053
ngjuping <ngjuping2018@163.com>
2020-12-19 01:13:02 +0800

    feat: map.py better cli arg interface doc: update readme with zmap command, cidrs, etc.

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # NetworkTopoTHU
 
-## How to run
+## How to run visualization
 Under the view directory, run
 ```
  python -m http.server 8080
 ```
+
+## How to run map.py
+```
+python3 map.py -i data/1.csv -o 1.json -g 1 --skip 500
+```
+
+## Target IP CIDRs
+- 59.66.0.0/16 
+- 101.5.0.0/16 
+- 101.6.0.0/16 
+- 118.229.0.0/19 
+- 166.111.0.0/16 
+- 183.172.0.0/15 
+- 202.112.39.2/32 
+- 219.223.168.0/21 
+- 219.223.176.0/20 
+
+## Zmap ip ping test command
+`sudo zmap --probe-module=icmp_echoscan -B 10M -o results.csv 101.5.0.0/16`
+
--- map.py
+++ map.py
@@ -5,38 +5,57 @@ from networkx.readwrite import json_graph
 import csv 
 import json
 import sys
+from optparse import OptionParser
 
-# First argument(x) = n-th network block
-# Second argument(n) = read once every x traversed
+# Initialize the command line parser instance
+parser = OptionParser()
+parser.add_option("-i", "--input", dest="in_filename",
+                  help="read from file for ip to traceroute")
+parser.add_option("-o", "--output", dest="out_filename",
+                  help="write to file the json for d3js visualization")
+parser.add_option("-g", "--group", dest="network_group",
+                  help="group for the d3.js nodes")
+parser.add_option("--skip", metavar="N", dest="operate_every_n", help="Traceroute every N ips")
 
-file_name = sys.argv[1]
-network_group = int(sys.argv[2])
-ip_addrs = csv.reader(open(file_name,'r'))
+(options, args) = parser.parse_args()
 
-# 每n个跳过
-operate_every_n = int(sys.argv[3])
+# Get parsed command line argument
+in_filename = options.in_filename
+out_filename = options.out_filename
+network_group = int(options.network_group)
+operate_every_n = int(options.operate_every_n)
+
+# Read a series of ips to be traceroute'ed
+ip_addrs = csv.reader(open(in_filename,'r'))
+
+# Counter that is used for traceroute every n ips
 counter_for_skip = 1
-
-# Data for testing
-# targets = ['101.5.224.77','101.5.3.33','166.111.79.129','166.111.68.150','59.66.217.10','59.66.212.8','118.229.25.226']
 
 G = nx.Graph()
 
 for raw_ip in ip_addrs:
+    
 
     # Only perform traceroute every n, as specified in the cmd argument
     if(counter_for_skip < operate_every_n):
         counter_for_skip += 1
         continue
-
-    # Reset counter
-    counter_for_skip = 1
+    else:
+        # Reset counter
+        counter_for_skip = 1
 
     ip = raw_ip[0]
+    
+    print('tracert ' + ip, end="... ")
+
+    # Prevent OS buffering, otherwise done is printed before os.system(command)
+    sys.stdout.flush()
+
     command = "traceroute -I %s > data.txt" % (ip)
+
     os.system(command)
 
-    print('tracert ' + ip + ' done')
+    print('done')
 
     with open('data.txt', 'r') as reader:
         reader.readline()  # Read off heading
@@ -44,10 +63,9 @@ for raw_ip in ip_addrs:
         previous_ip = None
         line = reader.readline()
         while( line ):
-            x = re.search(r"\(([\d.]+)\)", line)
-            if x:
-                current_ip = x.group(1)
-                # print(current_ip)
+            match = re.search(r"\(([\d.]+)\)", line)
+            if match:
+                current_ip = match.group(1)
                 if current_ip not in G:
                     G.add_node(current_ip,group=network_group)
                 if previous_ip:
@@ -56,8 +74,10 @@ for raw_ip in ip_addrs:
 
             line = reader.readline()
 
+os.remove('data.txt')
+
 nx_graph_dict = json_graph.node_link_data(G)
 
-with open("data_for_d3js.json","w") as json_save_to_file:
+with open(out_filename,"w") as json_save_to_file:
     nx_graph_json = json.dump(nx_graph_dict,json_save_to_file)
 
