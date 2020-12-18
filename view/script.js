5ec82cbbb24c6b4dbd2533a3663f40a853363723
ngjuping <ngjuping2018@163.com>
2020-12-19 01:22:31 +0800

    Refactor: Change file structure

--- view/script.js
+++ view/script.js
@@ -1,103 +1,115 @@
 // https://observablehq.com/@d3/force-directed-graph@149
 export default function define(runtime, observer) {
   const main = runtime.module();
-  const fileAttachments = new Map([["miserables.json",new URL("./data_for_d3js",import.meta.url)]]);
-  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
-  main.variable(observer()).define(["md"], function(md){return(
-md`# Force-Directed Graph
+  const fileAttachments = new Map([
+    ["miserables.json", new URL("./data_for_d3js.json", import.meta.url)],
+  ]);
+  main.builtin(
+    "FileAttachment",
+    runtime.fileAttachments((name) => fileAttachments.get(name))
+  );
+  main.variable(observer()).define(["md"], function (md) {
+    return md`
+# Force-Directed Graph
 
-This network of character co-occurence in _Les Misérables_ is positioned by simulated forces using [d3-force](https://github.com/d3/d3-force). See also a [disconnected graph](/@d3/disjoint-force-directed-graph), and compare to [WebCoLa](/@mbostock/hello-cola).`
-)});
-  main.variable(observer("chart")).define("chart", ["data","d3","width","height","color","drag","invalidation"], function(data,d3,width,height,color,drag,invalidation)
-{
-  console.log(data);
-  const links = data.links.map(d => Object.create(d));
-  const nodes = data.nodes.map(d => Object.create(d));
-  console.log(nodes);
-  const simulation = d3.forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id))
-      .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2));
-  console.log(nodes);
-  const svg = d3.create("svg")
-      .attr("viewBox", [0, 0, width, height]);
-  const link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links)
-    .join("line")
-      .attr("stroke-width", d => Math.sqrt(d.value));
-
-  const node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .join("circle")
-      .attr("r", 5)
-      .attr("fill", color)
-      .call(drag(simulation));
-
-  node.append("title")
-      .text(d => d.id);
-  simulation.on("tick", () => {
-    link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
-
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
+This network of character co-occurence in _Les Misérables_ is positioned by simulated forces using [d3-force](https://github.com/d3/d3-force). See also a [disconnected graph](/@d3/disjoint-force-directed-graph), and compare to [WebCoLa](/@mbostock/hello-cola).`;
   });
+  main
+    .variable(observer("chart"))
+    .define(
+      "chart",
+      ["data", "d3", "width", "height", "color", "drag", "invalidation"],
+      function (data, d3, width, height, color, drag, invalidation) {
+        console.log(data);
+        const links = data.links.map((d) => Object.create(d));
+        const nodes = data.nodes.map((d) => Object.create(d));
+        console.log(nodes);
+        const simulation = d3
+          .forceSimulation(nodes)
+          .force(
+            "link",
+            d3.forceLink(links).id((d) => d.id)
+          )
+          .force("charge", d3.forceManyBody())
+          .force("center", d3.forceCenter(width / 2, height / 2));
+        console.log(nodes);
+        const svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
+        const link = svg
+          .append("g")
+          .attr("stroke", "#999")
+          .attr("stroke-opacity", 0.6)
+          .selectAll("line")
+          .data(links)
+          .join("line")
+          .attr("stroke-width", (d) => Math.sqrt(d.value));
 
-  invalidation.then(() => simulation.stop());
+        const node = svg
+          .append("g")
+          .attr("stroke", "#fff")
+          .attr("stroke-width", 1.5)
+          .selectAll("circle")
+          .data(nodes)
+          .join("circle")
+          .attr("r", 5)
+          .attr("fill", color)
+          .call(drag(simulation));
 
-  return svg.node();
-}
-);
-  main.variable(observer("data")).define("data", ["FileAttachment"], function(FileAttachment){return(
-FileAttachment("miserables.json").json()
-)});
-  main.variable(observer("height")).define("height", function(){return(
-600
-)});
-  main.variable(observer("color")).define("color", ["d3"], function(d3)
-{
-  const scale = d3.scaleOrdinal(d3.schemeCategory10);
-  return d => scale(d.group);
-}
-);
-  main.variable(observer("drag")).define("drag", ["d3"], function(d3){return(
-simulation => {
-  
-  function dragstarted(event) {
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    event.subject.fx = event.subject.x;
-    event.subject.fy = event.subject.y;
-  }
-  
-  function dragged(event) {
-    event.subject.fx = event.x;
-    event.subject.fy = event.y;
-  }
-  
-  function dragended(event) {
-    if (!event.active) simulation.alphaTarget(0);
-    event.subject.fx = null;
-    event.subject.fy = null;
-  }
-  
-  return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
-}
-)});
-  main.variable(observer("d3")).define("d3", ["require"], function(require){return(
-require("d3@6")
-)});
+        node.append("title").text((d) => d.id);
+        simulation.on("tick", () => {
+          link
+            .attr("x1", (d) => d.source.x)
+            .attr("y1", (d) => d.source.y)
+            .attr("x2", (d) => d.target.x)
+            .attr("y2", (d) => d.target.y);
+
+          node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+        });
+
+        invalidation.then(() => simulation.stop());
+
+        return svg.node();
+      }
+    );
+  main
+    .variable(observer("data"))
+    .define("data", ["FileAttachment"], function (FileAttachment) {
+      return FileAttachment("miserables.json").json();
+    });
+  main.variable(observer("height")).define("height", function () {
+    return 600;
+  });
+  main.variable(observer("color")).define("color", ["d3"], function (d3) {
+    const scale = d3.scaleOrdinal(d3.schemeCategory10);
+    return (d) => scale(d.group);
+  });
+  main.variable(observer("drag")).define("drag", ["d3"], function (d3) {
+    return (simulation) => {
+      function dragstarted(event) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+      }
+
+      function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      }
+
+      function dragended(event) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+      }
+
+      return d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+    };
+  });
+  main.variable(observer("d3")).define("d3", ["require"], function (require) {
+    return require("d3@6");
+  });
   return main;
 }
